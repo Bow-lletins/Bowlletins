509da915b02865a4bee306387edfe1d230c9687d
Tamelab <TAMELAB@HAWAII.EDU>
2026-04-14 23:00:47 -1000

    Update dbActions.ts

--- src/lib/dbActions.ts
+++ src/lib/dbActions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { Major} from '@prisma/client';
+import { Major } from '@prisma/client';
 import { hash } from 'bcrypt';
 import { redirect } from 'next/navigation';
 import { prisma } from './prisma';
@@ -24,34 +24,37 @@ export async function createUser(credentials: {
       email: credentials.email,
       password: hashedPassword,
       fullName: displayName,
-      major: 'Other'as Major,
+      major: 'Other' as Major,
       image: `https://ui-avatars.com/api/?name=${encodeURIComponent(credentials.fullName)}&background=random&color=fff&size=128`,
       role: 'USER',
     },
   });
+
   redirect('/auth/signin');
 }
 
-export async function updateProfile(data: { 
+export async function updateProfile(data: {
   email: string;
-  fullName?: string| null; 
-  major?: Major|null;
+  fullName?: string | null;
+  major?: Major | null;
   image?: string | null;
 }) {
   if (!data.email) return;
 
-  const updates: { 
-    fullName?: string; 
-    major?: Major; 
-    image?: string | null 
+  const updates: {
+    fullName?: string;
+    major?: Major;
+    image?: string | null;
   } = {};
 
-  if (data.fullName !== undefined && data.fullName !==null){
+  if (data.fullName !== undefined && data.fullName !== null) {
     updates.fullName = data.fullName;
-  } 
+  }
+
   if (data.major !== undefined && data.major !== null) {
     updates.major = data.major;
   }
+
   if (data.image !== undefined) {
     updates.image = data.image;
   }
@@ -64,14 +67,9 @@ export async function updateProfile(data: {
   revalidatePath('/homeDashboard');
 }
 
-
-/**
- * Changes the password of an existing user in the database.
- * @param credentials, an object with the following properties: email, password.
- */
 export async function changePassword(credentials: { email: string; password: string }) {
-  // console.log(`changePassword data: ${JSON.stringify(credentials, null, 2)}`);
   const password = await hash(credentials.password, 10);
+
   await prisma.user.update({
     where: { email: credentials.email },
     data: {
@@ -80,3 +78,20 @@ export async function changePassword(credentials: { email: string; password: str
   });
 }
 
+export async function addStuff(data: {
+  name: string;
+  quantity: number;
+  condition: 'excellent' | 'good' | 'fair' | 'poor';
+  owner: string;
+}) {
+  await prisma.stuff.create({
+    data: {
+      name: data.name,
+      quantity: Number(data.quantity),
+      condition: data.condition,
+      owner: data.owner,
+    },
+  });
+
+  revalidatePath('/list');
+}
